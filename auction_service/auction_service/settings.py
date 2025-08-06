cf61bdaae90f8ca17bd486d45f4420146f2e76cb
rajsrimann15 <rajsrimannwork@gmail.com>
2025-08-06 20:11:46 +0530

    update: auction page

--- auction_service/auction_service/settings.py
+++ auction_service/auction_service/settings.py
@@ -133,7 +133,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # your React frontend
     "http://127.0.0.1:5173",
-    'https://farmer-portal-gateway.onrender.com',
+    "https://farmer-portal-gateway.onrender.com",
     
 ]
 # If you're using cookies or Authorization header in fetch(), add:
